2b7361111b8768d6f4988440f9b55e9dda618983
ReneChl <rene258chl@gmail.com>
2025-06-25 15:58:42 -0300

    tcc rene
    tcc rene

---
+++ R00/R02.js
@@ -0,0 +1,9 @@
+const slides = document.querySelector('.slides');
+const totalSlides = slides.children.length;
+let index = 0;
+
+
+setInterval(() => {
+    index = (index + 1) % totalSlides;
+    slides.style.transform = `translateX(-${index * 100}%)`;
+},3000)
